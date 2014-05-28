32c302e1f48e4ca9d937347f35120282c27dd2c7
Ken Leung <mailtokenl@gmail.com>
2014-05-27 19:32:38 -0700

    kl

--- src/web/main/scripts/site.js
+++ src/web/main/scripts/site.js
@@ -13,6 +13,51 @@
 (function(undef){ "use strict"; var global = this, _ = global._ , $ = global.jQuery;
 var skaro = global.SkaroJS;
 
+function initCarousel() {
+  // games list carousel
+  $('.carousel').slick({
+    infinite: true,
+    slidesToShow: 2,//4,
+    slidesToScroll: 1,
+    slide: 'div',
+    dots: true,
+    easing: 'easeInOutQuart',
+    speed: 800,
+    responsive: [{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        easing: 'easeInOutQuart',
+        speed: 800,
+      }
+    },{
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        easing: 'easeInOutQuart',
+        speed: 800,
+      }
+    },{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        easing: 'easeInOutQuart',
+        speed: 800,
+      }
+    }]
+  });
+
+}
+
 function boot() {
 
   var em = $('html');
@@ -48,6 +93,9 @@ function boot() {
   $('body').waypoint( function (dir) {
     $('.navbar').toggleClass('minified dark-menu');
   }, { offset: '-200px' });
+
+
+  initCarousel();
 
   // show "back to top" button
   $(document).scroll( function () {
