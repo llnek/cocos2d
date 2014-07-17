95aff5c4d521e7dce2ccca4c752446c9c2b0c769
Ken Leung <mailtokenl@gmail.com>
2014-07-16 18:01:03 -0700

    kl

---
+++ src/main/clojure/cmzlabclj/frigga/pong/core.clj
@@ -0,0 +1,44 @@
+;; This library is distributed in  the hope that it will be useful but without
+;; any  warranty; without  even  the  implied  warranty of  merchantability or
+;; fitness for a particular purpose.
+;; The use and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0  (http://opensource.org/licenses/eclipse-1.0.php)  which
+;; can be found in the file epl-v10.html at the root of this distribution.
+;; By using this software in any  fashion, you are agreeing to be bound by the
+;; terms of this license. You  must not remove this notice, or any other, from
+;; this software.
+;; Copyright (c) 2013-2014 Cherimoia, LLC. All rights reserved.
+
+
+(ns ^{:doc ""
+      :author "kenl"}
+
+  cmzlabclj.frigga.pong.core
+
+  (:require [clojure.tools.logging :as log :only [info warn error debug] ]
+            [clojure.string :as cstr])
+
+  (:use [cmzlabclj.nucleus.util.core :only [MakeMMap ternary notnil? ] ]
+        [cmzlabclj.nucleus.util.str :only [strim nsb hgl?] ])
+
+  (:use [cmzlabclj.cocos2d.games.meta])
+
+  (:import  [com.zotohlab.odin.game Game PlayRoom Player PlayerSession Session]
+            [com.zotohlab.odin.event EventDispatcher]))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+
+
+(deftype Pong [] com.zotohlab.odin.game.GameEngine
+
+  (update [_ evt]
+    (log/debug "Pong received event " evt))
+
+  (state [_] nil))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+(def ^:private core-eof nil)
+
